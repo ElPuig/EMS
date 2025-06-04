347afc7573fd7875298f4627da3b1fc1aeb7b707
FherStk <porrino.fernando@gmail.com>
2025-06-04 17:43:28 +0000

    Studies and ponderation added to the planning.

--- models/planning/planning.py
+++ models/planning/planning.py
@@ -11,23 +11,27 @@ class ims_planning(models.Model):
     ]
 
 	#TODO: For now, only for grading ponderation. 
-	
 	#TODO:	Multiple teachers as planning redactors.
 	#		Needed a review and approval system (like minutes will do).
 	# teacher_id = fields.Many2one(string="Teacher", comodel_name="hr.employee", required=True, domain="[('employee_type', '=', 'teacher')]")
-	name = fields.Char(string="Name", related='subject_id.display_name', store=True)
-	subject_id = fields.Many2one(string="Subject", comodel_name="ims.subject", required=True)
-	#TODO: also studies! (same subject but for different studies has different plannings)
+	name = fields.Char(string="Name", compute="_compute_name", store=True)
+	study_id = fields.Many2one(string="Study", comodel_name="ims.study", required=True)	
+	subject_id = fields.Many2one(string="Subject", comodel_name="ims.subject", required=True)	
+	allowed_subject_ids = fields.Many2many(related="study_id.subject_ids", store=False) 
 	planning_outcome_ids = fields.One2many(string="Outcome ponderation", comodel_name="ims.planning_outcome", inverse_name="planning_id")
+	internal_ponderation = fields.Float(string="Internal grading ponderation (%)", default=90.0, required=True)
+	external_ponderation = fields.Float(string="External grading ponderation (%)", default=10.0, required=True)
 
-	@api.constrains("planning_outcome_ids")
+	@api.constrains("planning_outcome_ids", "internal_ponderation", "external_ponderation")
 	def check_ponderation(self):
 		for rec in self:
 			total = 0
 			for pc in rec.planning_outcome_ids:
 				total += pc.ponderation			
 			if round(total,2) != 100:
-				raise ValidationError("The ponderation values must sum 100.")
+				raise ValidationError("The outcome ponderation values must sum 100.")
+			if round(rec.internal_ponderation + rec.external_ponderation, 2) != 100:
+				raise ValidationError("The main ponderation values must sum 100.")
 	
 	@api.onchange('subject_id')
 	def _onchange_planning_outcome_ids(self):
@@ -46,4 +50,9 @@ class ims_planning(models.Model):
 							"outcome_id": oc.id,
 							"ponderation": pond if i < count - 1 else last
 						})]
-					})                           
+					})
+
+	@api.depends("study_id", "subject_id")
+	def _compute_name(self):			
+		for rec in self:						
+			rec.name = "%s  %s" % (rec.study_id.acronym, rec.subject_id.display_name)

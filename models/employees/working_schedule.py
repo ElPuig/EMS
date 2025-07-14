e333ab52e64cc2da4d8d3864963f2379f47953df
FherStk <porrino.fernando@gmail.com>
2025-07-14 12:42:46 +0000

    Parsing data, work in progress

--- models/employees/working_schedule.py
+++ models/employees/working_schedule.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import xml.etree.ElementTree as ET
+import base64
 
 class ims_working_schedule(models.Model):
 	_inherit = 'resource.calendar'
@@ -17,4 +19,45 @@ class ims_working_schedules_import_wizard(models.TransientModel):
 	file = fields.Binary(string="Planner file (XML)", related="attachment_id.datas")
 
 	def import_planner_data(self):	
-		raise ValidationError("HOLA")
+		for rec in self:			
+			raise ValidationError("HOLA")
+		
+	@api.model_create_multi
+	def create(self, values):
+		for item in values:
+			if 'file' not in item or not item.get('file'):
+				raise ValidationError("No XML file has been loaded. Please, provide an XML file and try again.")
+			else:				
+				file = item.get('file')
+				xml_content = base64.b64decode(file)
+				tree = ET.ElementTree(ET.fromstring(xml_content))
+				
+				root = tree.getroot()
+				for teacherNode in root:					
+					email = teacherNode.attrib['name'].split(' ')[0]
+					# TODO: find the teacher using the email
+					
+					for dayNode in teacherNode:
+						number = dayNode.attrib['name'].split(' ')[0]
+						# TODO: convert the number to the proper object (1: Monday, 2: Tuesday...)
+												
+						for hourNode in dayNode:
+							# Ignore empty hours (lack of activities)
+							id = None
+							subject = None
+							group = None
+							
+							for content in hourNode:
+								if content.tag == 'Activity':
+									id = content.attrib['id'].split(' ')[0]
+								elif content.tag == 'Subject':
+									subject = content.attrib['name'].split(' ')[0]
+								elif content.tag == 'Students':
+									group = content.attrib['name'].split(' ')[0]														
+							
+							if id is not None:
+								# TODO: load the subject and the group
+								fake = 0			
+
+		return None
+				

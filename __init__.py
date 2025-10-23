651ef26f43ed982d58e52402cfc7f0dc9f6658b4
FherStk <porrino.fernando@gmail.com>
2025-10-23 15:56:47 +0000

    Port increase if in use (because with 2 workers, 2 debugpies runs).

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 import debugpy
-debugpy.listen(("0.0.0.0", 5678))
+import socket
+
+while True:
+    port = 5678
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if s.connect_ex(('localhost', port)) == 0:
+            port += 1            
+        else:
+            debugpy.listen(("0.0.0.0", port))
+            break        
 
 from . import controllers
 from . import models

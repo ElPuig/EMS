ea56ba25ae64041fbd8eec3c13a089dee325fe71
FherStk <porrino.fernando@gmail.com>
2025-10-24 11:11:40 +0000

    Disabled debugpy and added the call to the odoo.service.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import debugpy
-import os
+# import debugpy
+# import os
 
-is_worker = bool(os.environ.get('ODOO_WORKER_TYPE'))
-if not is_worker:
-    debugpy.configure({"subProcess": True})
+# is_worker = bool(os.environ.get('ODOO_WORKER_TYPE'))
+# if not is_worker:
+#     debugpy.configure({"subProcess": True})
 
-    try:
-        debugpy.listen(("0.0.0.0", 5678))
-    except RuntimeError as e:
-        print(f"   ERROR running debugpy: {e}.")        
+#     try:
+#         debugpy.listen(("0.0.0.0", 5678))
+#     except RuntimeError as e:
+#         print(f"   ERROR running debugpy: {e}.")        
 
 from . import controllers
 from . import models

afe8065f6702e3e31e9903ac4dcb119fd959810f
Fher <porrino.fernando@gmail.com>
2025-10-31 15:50:23 +0100

    18.0.0.1.4
    * Fixed: the current session is retreived. Existing sessions must be updated (just by changing its start-date).
    
    * Fix: status entries were being removed when filtering the jkustifications (when appearing and disappearing due filter).
    
    * Improvement: write method used only when mandatory (setting up attribute value instead using tuples).

--- models/shared/course.py
+++ models/shared/course.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import pytz
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from datetime import datetime
@@ -7,13 +8,14 @@ from datetime import datetime
 class ems_course(models.Model):
 	_name = "ems.course"
 	_description = "Course: defines a range of time when a course is running (for example: 2024-2025)."
+	_inherit = ['ems.utils']
 	_sql_constraints = [
 		('unique_course_name', 'unique (name)', 'duplicated course!')
     ]
 	
 	name = fields.Char(string="Name", compute="_compute_name", store=True)
-	start = fields.Integer(string="Start", default=datetime.now().year, required=True)
-	end = fields.Integer(string="End", default=datetime.now().year+1, required=True)	
+	start = fields.Integer(string="Start", default=lambda self: self.get_current_datetime().year, required=True)
+	end = fields.Integer(string="End", default=lambda self: self.get_current_datetime().year+1, required=True)	
 
 	@api.depends("start", "end")
 	def _compute_name(self):			

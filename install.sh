572310a011151389e05d897215ee0811c61c28e9
FherStk <porrino.fernando@gmail.com>
2025-11-07 10:53:41 +0000

    --i18n-overwrite

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing the EMS..."
 sudo service odoo stop
-sudo -u odoo bash -c 'odoo -d ems --stop-after-init -i ems -c /etc/odoo/odoo.conf --without-demo=WITHOUT_DEMO'
+sudo -u odoo bash -c 'odoo -d ems --stop-after-init -i ems --i18n-overwrite -c /etc/odoo/odoo.conf --without-demo=WITHOUT_DEMO'
 sudo service odoo start
 echo "Done!"

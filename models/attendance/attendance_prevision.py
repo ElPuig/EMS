4bb8f7382c12426c4d674abe628a3fff45c0160a
FherStk <porrino.fernando@gmail.com>
2026-01-21 19:06:49 +0000

    setting up prevission create and unlink methods

--- models/attendance/attendance_prevision.py
+++ models/attendance/attendance_prevision.py
@@ -21,53 +21,29 @@ class ems_attendance_prevision(models.Model):
 	@api.model_create_multi
 	def create(self, values):		
 		just = super().create(values)
-		for val in values:
-			is_admin = just.get_user_is_admin()
-			is_tutor = just.get_user_is_tutor_of_self()							
-			
-			if not is_admin and not is_tutor:
-				# TODO: localize the message
-				raise ValidationError("Only the student's tutor can create attendance previsions.")
-			
-			# if 'attendance_status_ids' in val and val.get('attendance_status_ids'):
-			# 	# NOTE: it would be nice to have a link to the justification entry within the status form, but this demands an 
-			# 	# extra column that will be almost always empty. So, at the moment, the text "Justified by" will be added to the
-			# 	# comments sections. 
+		is_admin = just.get_user_is_admin()
+		is_tutor = just.get_user_is_tutor_of_self()							
+		
+		if not is_admin and not is_tutor:
+			# TODO: localize the message
+			raise ValidationError("Only the student's tutor can create attendance previsions.")
 				
-			# 	# TODO: localize text
-			# 	text = "Justified by: " + just.teacher_id.display_name
-			# 	for status in just.attendance_status_ids:	
-			# 		if status.status == "m_miss":			
-			# 			notes = "" if status.notes == False else status.notes + "\n"
-			# 			notes += text
-			# 			status.write({
-			# 				'status': 'm_justified',
-			# 				'notes': notes
-			# 			}) 		
 		return just
 	
 	def unlink(self):
-		# for rec in self:
-		# 	if not rec.user_is_admin and not rec.student_id.main_group_id in rec.teacher_id.tutorship_ids:
-		# 		raise UserError("Only the studen's tutor can remove its attendance justifications.")
-		# 	else:
-		# 		for status in rec.attendance_status_ids:	
-		# 			if status.status == "m_justified":	
-		# 				# TODO: localize text
-		# 				text = "Justified by: " + rec.teacher_id.display_name		
-		# 				notes = False if status.notes == False else status.notes.replace(text, "")
-		# 				status.write({
-		# 					'status': 'm_miss',
-		# 					'notes': False if len(notes) == 0 else notes
-		# 				})
-		# 		rec.write({'attendance_status_ids' : [5]})	
-
-		return super().unlink()
 		for rec in self:
-			start_date = False if rec.start_date == False else rec.utc_datetime_to_local(rec.start_date)	
-			end_date = False if rec.end_date == False else rec.utc_datetime_to_local(rec.end_date)	
-			
-			if rec.student_id.id and start_date and end_date:
-				rec.display_name = "%s (from %02d:%02d to %02d:%02d)" % (rec.student_id.display_name, start_date.hour, start_date.minute, end_date.hour, end_date.minute)
+			if not rec.user_is_admin and not rec.student_id.main_group_id in rec.teacher_id.tutorship_ids:
+				raise UserError("Only the studen's tutor can remove its attendance previssions.")
 			else:
-				rec.display_name = False
+				for status in rec.attendance_status_ids:	
+					if status.status == "m_justified":	
+						# TODO: localize text
+						text = "Absence expected by: " + rec.teacher_id.display_name
+						notes = False if status.notes == False else status.notes.replace(text, "")
+						status.write({
+							'status': 'm_miss',
+							'notes': False if len(notes) == 0 else notes
+						})
+				rec.write({'attendance_status_ids' : [5]})	
+
+		return super().unlink()		

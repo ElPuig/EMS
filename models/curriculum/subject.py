c9576eb91e093f0b23368cf50b807425da200e31
Fher <porrino.fernando@gmail.com>
2026-04-08 10:43:29 +0200

    v18.0.0.6.1
    # Changes
    - New roles for teachers: _CatSkills coordinator_; _Seminar leader_ and
    _Secretary_.
    - _Department Chieff_ is no more unipersonal, also the department name
    has been removed from the role name.
    - Removed duped roles (within 'cat' and 'custom' data folders). 
    # Fixes
    - The subjects can be searched by its complete 'display name' (name or
    acronym).
    - The subject form layout has been fixed.

--- models/curriculum/subject.py
+++ models/curriculum/subject.py
@@ -4,7 +4,8 @@ from odoo import models, fields, api
 
 class ems_subject(models.Model):
     _name = "ems.subject"
-    _description = "Subject: The main item for a student's subject."
+    _description = "Subject: The main item for a student's subject."    
+    _rec_names_search = ['name', 'acronym']
     _order = "code asc"
     _sql_constraints = [
         ('unique_code', 'unique (code)', 'duplicated code!')
@@ -33,4 +34,4 @@ class ems_subject(models.Model):
     @api.depends('acronym', 'name')
     def _compute_display_name(self):               
         for rec in self:                
-            rec.display_name = "%s: %s" % (rec.acronym, rec.name)                
+            rec.display_name = "%s: %s" % (rec.acronym, rec.name)

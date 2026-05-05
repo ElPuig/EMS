c65bcf1e4aebbd50f3c2bd3a5a9cc2cfae1920b3
JuanMoroteP <juan.morote@gmail.com>
2026-05-06 00:11:20 +0200

    Add custom enrollment email template and PDF report
    - Add multilingual email template (ca/es/en) with company sender,
      portal link forced to /ca/, and no sign/accept button
    - Add custom PDF report "Print enrollment" replacing Odoo's default
      quotation reports in the print menu
    - Hide "Preview" button from enrollment form
    - Clear salesperson on enrollment creation so company name appears
      as sender fallback

--- __manifest__.py
+++ __manifest__.py
@@ -209,6 +209,8 @@
             'views/academic_management/enrollment_configuration/enrollment_authorization_view.xml',
             'views/academic_management/enrollment_configuration/enrollment_authorization_form.xml',
             'reports/authorizations/report_authorization_certificate.xml',
+        'reports/enrollment/templates/report_enrollment_template.xml',
+        'reports/enrollment/enrollment.xml',
             'views/academic_management/enrollment_configuration/enrollment_template_form.xml',
             'views/academic_management/enrollment_configuration/enrollment_template_view.xml',
             'views/academic_management/enrollment_configuration/menu.xml',
@@ -232,9 +234,10 @@
 
 
         ### Mailing templates ###
-        'mails/attendance/attendance_issue_status.xml', 
-        'mails/attendance/attendance_issue_rectification.xml', 
-        'mails/attendance/attendance_issue_tutor.xml', 
+        'mails/attendance/attendance_issue_status.xml',
+        'mails/attendance/attendance_issue_rectification.xml',
+        'mails/attendance/attendance_issue_tutor.xml',
+        'mails/enrollment/enrollment_send.xml',
 
         ### Reports templates ###
         'reports/attendance/templates/sumary_table.xml', 
